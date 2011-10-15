410ae4a2fa131cfe29d08083c1d94d7dccf3ecd9
eSportsMexico <admin@esports.mx>
2011-10-15 02:05:35 -0500

    Modulo Equipo: Modificado bloque de juegos en los que participa 	para incluir el despliegue en equipos Modulo Match: Eliminado bloque de calificacion de equipo Modulo Rankiing: Agregada funcion para saber la division de un equipo

---
+++ team/team_info.tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to present the teams's info.
+ *
+ * Available variables:
+ * - $tid: Image set by the user or the site's default. Will use
+ * - $ranking: TRUE if imagecache was used to size the picture. This
+ * - $division: TRUE if imagecache was used to size the picture. This
+ * - $calificacion:
+ *   tells us if we want to link to the full sized image.
+ *
+ */
+?>
+
+<div class = "fondo_juegos">
+  <?php print '<div class = "left_'. str_replace( " ", "_", $modalidad->Nombre) .'"></div>'; ?>
+  <div class = "datos_equipo">
+      <ul>
+          <?php print '<li>'. l($modalidad->Nombre, 'taxonomy/term/'. $modalidad->tid) .'</li>'; ?>
+          <?php print '<li>'. l($team->title, 'node/'. $team->nid) .'</li>';  ?>
+          <?php print '<li>'. t('Ranking: '). $ranking .'</li>'; ?>
+          <?php print '<li>'. t('Division: '). $division .'</li>'; ?>
+          <?php print '<li>'. $calificacion .'</li>'; ?>
+      </ul>
+  </div>
+  <?php print '<div class = "right_'. str_replace( " ", "_", $modalidad->Nombre) .'"></div>'; ?>
+</div>
+
